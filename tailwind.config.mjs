bfb88561c721f26bcd542d1635adc8ed6936c849
narmanguebraun <narmanguebraun1@gmail.com>
2024-02-09 03:00:37 +0100

    Add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Geist", "Helvetica", "sans-serif"],
+      mono: ["GeistMono", "Courier", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
