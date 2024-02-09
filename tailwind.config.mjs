b8a3cf67c9193cf8268322437b327a08e4d74b55
narmanguebraun <narmanguebraun1@gmail.com>
2024-02-09 19:59:37 +0100

    Change main font,  add colors to theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,8 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      inherit: "inherit",
+      white: "#fff",
+      light: "#f2f2f2",
+      lightgrey: "#e5e5e5",
+      grey1: "#111",
+      grey2: "#555",
+      grey3: "#999",
+    },
     fontFamily: {
-      sans: ["Geist", "Helvetica", "sans-serif"],
+      sans: ["Inter", "Helvetica", "sans-serif"],
       mono: ["GeistMono", "Courier", "monospace"],
     },
     extend: {},
